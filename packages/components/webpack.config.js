b11c115694418452bdce201da3e2efa0f32d91d0
ionel <ionel@ima.ro>
2022-11-25 18:02:40 +0200

    webpack config

--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -1,35 +1,47 @@
 const path = require("path")
 
-module.exports = {
-    mode: "production",
-    entry: {
-        index: { import: "./src/index.js" }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        '@babel/preset-env',
-                        '@babel/react',
-                        {
-                            'plugins': ['@babel/plugin-proposal-class-properties']
-                        }
-                    ]
+module.exports = (env) => {
+    // console.log('Goal: ',env.goal, env);
+    // console.log('Production: ', !!env.production)
 
-                }
+    return {
+        //mode: !!env.production ? 'production':'development',
+        mode: 'production',
+
+        entry: {
+            components: './src/index.js'
+        },
+
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/react'
+                        ]
+
+                    }
+                },
+            ],
+        },
+
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            library: {
+                type: 'umd'
             },
-        ],
-    },
-    output: {
-        //path: path.resolve(__dirname, 'lib'),
-        filename: "components.bundle.min.js",
-        library: 'imaComponents',
-        libraryTarget: 'umd',
-        clean: true
-    },
-    externals: {react: 'react'}
+            clean: true
+        },
+
+        externals: { react: 'react' }
+    }
 }
